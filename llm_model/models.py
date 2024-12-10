acb77da68755b44303eeec00cfa08e503c8d670a
Eyphiriam <150979401+Eyphiriam@users.noreply.github.com>
2024-12-09 22:57:16 -0500

    Delete llm_model directory

--- llm_model/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ImageUpload(models.Model):
-    image = models.ImageField(upload_to='uploads/')  # Adjust 'uploads/' as needed
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.imag
